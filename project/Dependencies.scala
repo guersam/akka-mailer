220cec771fe5035c415241a498b4cdbde22200eb
pjan vandaele <pjan@pjan.io>
2014-07-18 14:10:56 +0900

    Updated dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,13 +5,7 @@ object Dependencies {
   object Versions {
     val akka                = "2.3.4"
     val apacheCommonsEmail  = "1.3.2"
-    val aspectj             = "1.7.4"
     val scalajHttp          = "0.3.16"
-    val kamon               = "0.3.1"
-    val logback             = "1.1.2"
-    val play                = "2.3.0"
-    val slick               = "2.1.0-M2"
-    val spray               = "1.3.1"
     val typesafeConfig      = "1.2.0"
 
     val scalaTest       = "2.2.0"
@@ -20,10 +14,8 @@ object Dependencies {
   }
 
   val akkaActor           = "com.typesafe.akka"   %%  "akka-actor"                  % Versions.akka
-  val akkaSlf4j           = "com.typesafe.akka"   %%  "akka-slf4j"                  % Versions.akka
   val akkaTest            = "com.typesafe.akka"   %%  "akka-testkit"                % Versions.akka
   val apacheCommonsEMail  = "org.apache.commons"  %   "commons-email"               % Versions.apacheCommonsEmail
-  val logback             = "ch.qos.logback"      %   "logback-classic"             % Versions.logback
   val scalajHttp          = "org.scalaj"          %%  "scalaj-http"                 % Versions.scalajHttp
   val typesafeConfig      = "com.typesafe"        %   "config"                      % Versions.typesafeConfig
 
